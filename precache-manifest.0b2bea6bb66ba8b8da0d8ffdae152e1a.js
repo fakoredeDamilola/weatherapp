5bbd9e9cf4896b101ee5b182f9c70599bf94a6a9
Fakorede Damilola <dfakorede29@gmail.com>
2020-08-01 22:17:05 +0100

    Updates

---
+++ precache-manifest.0b2bea6bb66ba8b8da0d8ffdae152e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9c41ad44c0dbcb8e82b403ae79cc62",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "cef991a032d0371e44fa",
+    "url": "/weatherapp/static/css/main.6c9288d9.chunk.css"
+  },
+  {
+    "revision": "42cb6c32227c3101a5d2",
+    "url": "/weatherapp/static/js/2.d26d0e39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.d26d0e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef991a032d0371e44fa",
+    "url": "/weatherapp/static/js/main.fde6b8b4.chunk.js"
+  },
+  {
+    "revision": "ff907cfbbb256dae02f8",
+    "url": "/weatherapp/static/js/runtime-main.f16cac52.js"
+  }
+]);
